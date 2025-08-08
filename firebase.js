af79b232e282d04f0b05e209c4076e48f34bece9
cariantohs <168389281+cariantohs@users.noreply.github.com>
2025-08-08 11:57:13 +0700

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,14 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBO3Bvg9xQzvhkArwMv-8tYgEtxoWR_XKY",
+  authDomain: "crypto-tracker-b3b6b.firebaseapp.com",
+  databaseURL: "https://crypto-tracker-b3b6b-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "crypto-tracker-b3b6b",
+  storageBucket: "crypto-tracker-b3b6b.firebasestorage.app",
+  messagingSenderId: "241465917079",
+  appId: "1:241465917079:web:3d031edf8ded704bb833d2",
+  measurementId: "G-X85Y9VKG13"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
